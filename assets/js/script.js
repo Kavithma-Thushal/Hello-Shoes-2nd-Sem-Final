5f9a8b502d61bf8c3cbb384a57bbc8000902c6d9
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2024-04-20 15:04:48 +0530

    Save Customer Refactored

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,25 @@
+/** Save Customer **/
 $('#btnSaveCustomer').click(function () {
-    Swal.fire({
-        position: 'bottom-end',
-        icon: 'success',
-        title: "Hello Shoes...!",
-        showConfirmButton: false,
-        timer: 2000
+    let cusId = $('#txtCustomerId').val();
+    let cusName = $('#txtCustomerName').val();
+    let cusAddress = $('#txtCustomerAddress').val();
+
+    let customerObj = {
+        id: cusId,
+        name: cusName,
+        address: cusAddress,
+    }
+
+    $.ajax({
+        url: 'http://localhost:8080/shoes/api/v1/customers',
+        method: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify(customerObj),
+        success: function (data) {
+            alert("Customer Saved Successfully...!");
+        },
+        error: function (xhr, status, error) {
+            alert("Customer Saved Error...!");
+        }
     });
 });
