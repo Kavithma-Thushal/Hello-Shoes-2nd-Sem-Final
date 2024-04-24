a11b42d9486454278909659ebb36b5575d838db5
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2024-04-24 21:22:33 +0530

    AdminPanel Table Validated

--- controllers/AdminPanelController.js
+++ controllers/AdminPanelController.js
@@ -1,23 +1,30 @@
 /** Admin Panel **/
 $('#btnSaveTotalSales').click(function () {
-    const adminPanelObject = {
-        totalSales: $('#txtTotalSales').val(),
-        totalProfit: $('#txtTotalProfit').val(),
-        mostSaleItem: $('#txtMostSaleItem').val(),
-        mostSaleItemPic: $('#txtMostSaleItemPic').val(),
-        mostSaleItemQty: $('#txtMostSaleItemQty').val()
-    };
+    const fileInput = $('#txtMostSaleItemPic')[0];
+    const file = fileInput.files[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
 
-    $.ajax({
-        url: baseURL + 'adminPanel',
-        method: 'POST',
-        contentType: 'application/json',
-        data: JSON.stringify(adminPanelObject),
-        success: function (resp) {
-            successAlert(resp);
-        },
-        error: function (resp) {
-            errorAlert(resp);
-        }
-    });
+    reader.onload = function () {
+        const adminPanelObject = {
+            totalSales: parseInt($('#txtTotalSales').val()),
+            totalProfit: parseFloat($('#txtTotalProfit').val()),
+            mostSaleItem: $('#txtMostSaleItem').val(),
+            mostSaleItemPic: reader.result,
+            mostSaleItemQty: parseInt($('#txtMostSaleItemQty').val())
+        };
+
+        $.ajax({
+            url: baseURL + 'adminPanel',
+            method: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify(adminPanelObject),
+            success: function (resp) {
+                successAlert(resp);
+            },
+            error: function (resp) {
+                errorAlert(resp);
+            }
+        });
+    };
 });
