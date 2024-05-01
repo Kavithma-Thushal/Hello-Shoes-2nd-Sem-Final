91e881ebc070cd3b09a22e9fe5bffb8f0640471b
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2024-05-01 14:32:39 +0530

    Customer Updated Successfully

--- controllers/CustomerController.js
+++ controllers/CustomerController.js
@@ -31,3 +31,38 @@ $('#btnSaveCustomer').click(function () {
         }
     });
 });
+
+/** Update Customer **/
+$('#btnUpdateCustomer').click(function () {
+    const customerObject = {
+        id: $('#txtCustomerId').val(),
+        name: $('#txtCustomerName').val(),
+        gender: $('#txtCustomerGender').val(),
+        dob: $('#txtCustomerDOB').val(),
+        level: $('#txtCustomerLoyaltyLevel').val(),
+        loyaltyDate: $('#txtCustomerLoyaltyDate').val(),
+        totalPoints: parseInt($('#txtCustomerTotalPoints').val()),
+        addressLine1: $('#txtCustomerAddressLine1').val(),
+        addressLine2: $('#txtCustomerAddressLine2').val(),
+        addressLine3: $('#txtCustomerAddressLine3').val(),
+        addressLine4: $('#txtCustomerAddressLine4').val(),
+        addressLine5: $('#txtCustomerAddressLine5').val(),
+        contactNo: $('#txtCustomerContactNo').val(),
+        email: $('#txtCustomerEmail').val(),
+        recentDate: new Date($('#txtCustomerRecentDate').val()).toISOString()
+    };
+
+    $.ajax({
+        url: baseURL + 'customer',
+        method: 'PUT',
+        contentType: 'application/json',
+        data: JSON.stringify(customerObject),
+        success: function (resp) {
+            successAlert(resp);
+        },
+        error: function (resp) {
+            errorAlert(resp);
+        }
+    });
+});
+
