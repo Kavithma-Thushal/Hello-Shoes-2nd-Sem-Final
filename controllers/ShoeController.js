87e5e7f42ceb43813bc922450fd18890b049a7ae
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2024-05-03 08:17:58 +0530

    Shoes CRUD Completed

--- controllers/ShoeController.js
+++ controllers/ShoeController.js
@@ -1,4 +1,4 @@
-/** Add Item **/
+/** Add Shoe **/
 $('#btnAddItem').click(function () {
     const fileInput = $('#txtItemPicture')[0];
     const file = fileInput.files[0];
@@ -23,7 +23,7 @@ $('#btnAddItem').click(function () {
         };
 
         $.ajax({
-            url: baseURL + 'shoe',
+            url: baseURL + 'item',
             method: 'POST',
             contentType: 'application/json',
             data: JSON.stringify(itemObject),
@@ -35,4 +35,77 @@ $('#btnAddItem').click(function () {
             }
         });
     };
+});
+
+/** Search Shoe **/
+$('#btnSearchItem').click(function () {
+    let code = $("#txtItemCode").val();
+
+    $.ajax({
+        url: baseURL + 'item/searchItem/' + code,
+        method: 'GET',
+        contentType: 'application/json',
+        success: function (resp) {
+            console.log("Shoe Searched Successfully...!");
+        },
+        error: function (resp) {
+            errorAlert(resp);
+        }
+    });
+});
+
+/** Update Shoe **/
+$('#btnUpdateItem').click(function () {
+    const fileInput = $('#txtItemPicture')[0];
+    const file = fileInput.files[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+
+    reader.onload = function () {
+        const itemObject = {
+            code: $('#txtItemCode').val(),
+            description: $('#txtItemDescription').val(),
+            picture: reader.result,
+            category: $('#txtItemCategory').val(),
+            size: parseInt($('#txtItemSize').val()),
+            quantity: parseInt($('#txtItemQuantity').val()),
+            supplierId: $('#txtSupplierIdd').val(),
+            supplierName: $('#txtSupplierNamee').val(),
+            unitPriceSale: parseFloat($('#txtUnitPriceSale').val()),
+            unitPriceBuy: parseFloat($('#txtUnitPriceBuy').val()),
+            expectedProfit: parseFloat($('#txtExpectedProfit').val()),
+            profitMargin: parseFloat($('#txtProfitMargin').val()),
+            status: $('#txtStatus').val()
+        };
+
+        $.ajax({
+            url: baseURL + 'item',
+            method: 'PUT',
+            contentType: 'application/json',
+            data: JSON.stringify(itemObject),
+            success: function (resp) {
+                successAlert(resp);
+            },
+            error: function (resp) {
+                errorAlert(resp);
+            }
+        });
+    };
+});
+
+/** Delete Shoe **/
+$('#btnDeleteItem').click(function () {
+    let code = $("#txtItemCode").val();
+
+    $.ajax({
+        url: baseURL + 'item/' + code,
+        method: 'DELETE',
+        contentType: 'application/json',
+        success: function (resp) {
+            successAlert(resp);
+        },
+        error: function (resp) {
+            errorAlert(resp);
+        }
+    });
 });
