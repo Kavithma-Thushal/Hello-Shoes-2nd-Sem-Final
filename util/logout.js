550a2acaeeac02ea973942e8e4321897ecff4a17
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2024-05-10 20:08:14 +0530

    Logout Refactored

--- util/logout.js
+++ util/logout.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
-    $('#logout').click(function (event) {
-        event.preventDefault();
+    $('#logout').click(function () {
         successAlert("User Logout Successfully...!");
-        document.cookie = 'jwtToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+        localStorage.removeItem('jwtToken');
+        // document.cookie = 'jwtToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
         window.location.href = '../index.html';
     });
 });
