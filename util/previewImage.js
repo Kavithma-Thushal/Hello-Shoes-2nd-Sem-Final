1c81bc619c0dc81acc731dfc0b7cb9e32f493bec
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2024-05-19 16:08:29 +0530

    Image Preview Added

---
+++ util/previewImage.js
@@ -0,0 +1,35 @@
+/**
+ * @author : Kavithma Thushal
+ * @project : Hello-Shoes-FrontEnd
+ * @since : 2:02 PM - 4/20/2024
+ **/
+
+/** Image Preview **/
+function previewImage(event) {
+    const input = event.target;
+    const preview = document.getElementById('shoeImagePreview');
+
+    if (input.files && input.files[0]) {
+        const reader = new FileReader();
+
+        reader.onload = function (e) {
+            preview.src = e.target.result;
+            preview.style.display = 'block';
+        };
+
+        reader.readAsDataURL(input.files[0]);
+    } else {
+        preview.src = '#';
+        preview.style.display = 'none';
+    }
+}
+
+/** Remove Preview when click the Add Shoe **/
+$('#btnAddShoe').on('click', function () {
+    const fileInput = $('#txtShoePicture');
+    fileInput.val('');
+
+    const preview = $('#shoeImagePreview');
+    preview.hide();
+    preview.attr('src', '#');
+});
