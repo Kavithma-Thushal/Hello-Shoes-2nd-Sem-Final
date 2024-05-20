7c0c9364bb3b7c994427ee4a4d9c2f6c70f31e4c
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2024-05-20 21:07:45 +0530

    Load All Customers to Combo-Box

---
+++ controllers/SalesController.js
@@ -0,0 +1,28 @@
+/**
+ * @author : Kavithma Thushal
+ * @project : Hello-Shoes-FrontEnd
+ * @since : 2:02 PM - 4/20/2024
+ **/
+
+/** Load All Customers to Combo-Box **/
+$.ajax({
+    url: baseURL + "customer/loadAllCustomers",
+    method: "GET",
+    headers: {
+        Authorization: 'Bearer ' + jwtToken
+    },
+    success: function (resp) {
+        $("#cmbCusId").empty();
+
+        // Add a default empty option
+        $("#cmbCusId").append('<option value="" disabled selected></option>');
+
+        for (let i of resp.data) {
+            let id = i.id;
+            $("#cmbCusId").append(`<option>${id}</option>`);
+        }
+    },
+    error: function (error) {
+        console.log(JSON.parse("Load All Customers to Combo-Box Error : " + error.responseText).message);
+    }
+});
